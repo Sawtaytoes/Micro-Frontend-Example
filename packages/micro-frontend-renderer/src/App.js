488b920224426cd088214ecbf0e762b41c80bf8a
Kevin Ghadyani <Saturn2888@gmail.com>
2020-02-12 00:36:02 -0600

    Added ability for iframe to change height

--- packages/micro-frontend-renderer/src/App.js
+++ packages/micro-frontend-renderer/src/App.js
@@ -1,16 +1,31 @@
-import React from 'react'
-import { BrowserRouter, Link, useLocation } from 'react-router-dom'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import { BrowserRouter, Link, Redirect, Route, Switch, useHistory } from 'react-router-dom'
 
 import './App.css'
 
 const MicroFrontendLoader = ({
   cssLocation,
-  matchingPathname,
+  microFrontendIdentifier,
   scriptLocations,
 }) => {
-  const location = useLocation()
+  const iframeRef = useRef()
+  const [iframeHeight, setIframeHeight] = useState()
 
-  const currentPathname = location.pathname.replace(/^(.*?)[/]*$/, '$1')
+  const onIframeHeightChanged = (
+    useCallback(() => {
+      window.requestAnimationFrame(() => {
+        setIframeHeight(iframeRef.current.contentDocument.body.clientHeight)
+      })
+    }, [iframeRef])
+  )
+
+  useEffect(() => {
+    window.addEventListener('resize', onIframeHeightChanged)
+
+    return () => {
+      window.removeEventListener('resize', onIframeHeightChanged)
+    }
+  }, [onIframeHeightChanged])
 
   return (
     `/${matchingPathname}` === currentPathname
@@ -35,10 +50,16 @@ const MicroFrontendLoader = ({
             </body>
           </html>
         `}
-        style={{
-          border: 0,
-          width: '100%',
-        }}
+      style={{
+        border: 'none',
+        display: 'block',
+        height: (
+          iframeHeight
+          ? `${iframeHeight}px`
+          : '100%'
+        ),
+        width: '100%',
+      }}
         title={matchingPathname}
       />
     )
@@ -62,25 +83,39 @@ const App = () => (
         </div>
       </nav>
 
-      <MicroFrontendLoader
-        cssLocation="/micro-frontend-1/main.eae2fd7b.chunk.css"
-        matchingPathname="micro-frontend-1"
-        scriptLocations={[
-          '/micro-frontend-1/2.23615124.chunk.js',
-          '/micro-frontend-1/main.90a2c267.chunk.js',
-          '/micro-frontend-1/runtime-main.7ec979d4.js',
-        ]}
-      />
+      <Switch>
+        <Route path="/micro-frontend-1">
+          <MicroFrontendLoader
+            cssLocation="/micro-frontend-1/main.a9054f16.chunk.css"
+            microFrontendIdentifier="micro-frontend-1"
+            scriptLocations={[
+              '/micro-frontend-1/2.23615124.chunk.js',
+              '/micro-frontend-1/main.90a2c267.chunk.js',
+              '/micro-frontend-1/runtime-main.7ec979d4.js',
+            ]}
+          />
+        </Route>
 
-      <MicroFrontendLoader
-        cssLocation="/micro-frontend-2/main.0a332295.chunk.css"
-        matchingPathname="micro-frontend-2"
-        scriptLocations={[
-          '/micro-frontend-2/2.b8170ca8.chunk.js',
-          '/micro-frontend-2/main.0f1b2201.chunk.js',
-          '/micro-frontend-2/runtime-main.0ef16878.js',
-        ]}
-      />
+        <Route path="/micro-frontend-2">
+          <MicroFrontendLoader
+            cssLocation="/micro-frontend-2/main.2e837e40.chunk.css"
+            microFrontendIdentifier="micro-frontend-2"
+            scriptLocations={[
+              '/micro-frontend-2/2.b8170ca8.chunk.js',
+              '/micro-frontend-2/main.0f1b2201.chunk.js',
+              '/micro-frontend-2/runtime-main.0ef16878.js',
+            ]}
+          />
+        </Route>
+
+        <Route path="/404">
+          <div className="App-unknownPathname">
+            404 - Path not found.
+          </div>
+        </Route>
+
+        <Redirect to="/404" />
+      </Switch>
 
       <footer className="App-header">
         Parent Document Footer
