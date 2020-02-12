91adfbddbf5393a0a25b2a8d7c191b87d73e304b
Kevin Ghadyani <Saturn2888@gmail.com>
2020-02-11 23:03:58 -0600

    WIP

---
+++ packages/micro-frontend-renderer/src/App.js
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react'
+
+import logo from './logo.svg'
+import './App.css'
+
+const useScript = url => {
+  useEffect(() => {
+    const script = document.createElement('script')
+
+    script.src = url
+    script.async = true
+
+    document.body.appendChild(script)
+
+    return () => {
+      document.body.removeChild(script)
+    }
+  }, [url])
+}
+
+// export default useScript
+
+const App = () => {
+  useScript('microFrontendJs/2.67ebc307.chunk.js')
+  useScript('microFrontendJs/main.976bc0aa.chunk.js')
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+
+      <div id="micro-frontend-1">1</div>
+      <div id="micro-frontend-2">2</div>
+    </div>
+  )
+}
+
+export default App
