0b1f329d40a4ebd652985ba62107a79e58a9799a
Kevin Ghadyani <Saturn2888@gmail.com>
2020-02-16 22:06:40 -0600

    Moved event dispatch to React in Micro Frontends

--- packages/micro-frontend-2/src/App.js
+++ packages/micro-frontend-2/src/App.js
@@ -3,7 +3,16 @@ import React from 'react'
 import './App.css'
 
 const App = () => (
-  <div className="App">
+  <div
+    className="App"
+    onClick={() => {
+      window
+      .top
+      .dispatchEvent(
+        new Event('unknownPath')
+      )
+    }}
+  >
     Micro Frontend 2
   </div>
 )
